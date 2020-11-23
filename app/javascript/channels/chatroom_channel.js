feeffc8b901c63c66e6c289240fb71d5bef6717d
andre ferrer <andrerferrer@hotmail.com>
2020-11-23 07:09:21 -0300

    refactor chatroom channel JS

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -8,24 +8,16 @@ const initChatroomCable = () => {
   const messagesContainer = document.getElementById('messages');
   // if we are on the chatroom page
   if (messagesContainer) {
-    // // we will connect to the chatroom
-    // const chatroomId = messagesContainer.dataset.chatroomId;
-    // const chatroom = { channel: "ChatroomChannel", id: chatroomId }
-    // const what_to_do = {
-    //   received(data) {
-    //     console.log(data); // called when data is broadcast in the cable
-    //   }
-    // }
-    // // connecting to the chatroom
-    // consumer.subscriptions.create(chatroom, what_to_do);
-    const id = messagesContainer.dataset.chatroomId;
-
-    consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
-      received(data) {
-        console.log(data); // called when data is broadcast in the cable
-      },
-    });
-
+    // we will connect to the chatroom
+    const chatroomId = messagesContainer.dataset.chatroomId;
+    const chatroom = { channel: "ChatroomChannel", id: chatroomId }
+    const what_to_do = {
+      received(messageHTML) {
+        messagesContainer.insertAdjacentHTML('beforeend', messageHTML);
+      }
+    }
+    // connecting to the chatroom
+    consumer.subscriptions.create(chatroom, what_to_do);
   }
 }
 
