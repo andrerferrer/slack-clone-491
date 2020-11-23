f1a66398cf9596c1b5cd1ba9872d61dd6d1f621e
andre ferrer <andrerferrer@hotmail.com>
2020-11-23 07:09:11 -0300

    Fix bug

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,7 +1,6 @@
-# 1. Create the Channel CORRECTLY unlike ANDRE LIVE FOR WORLD
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
     chatroom = Chatroom.find(params[:id])
-    stream_from chatroom
+    stream_for chatroom
   end
 end
